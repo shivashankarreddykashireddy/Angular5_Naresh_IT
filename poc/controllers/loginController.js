2fdfb4dd07a14f05c41d6dfe2d071f042cbde02d
Vinay <bharath.g9297@gmail.com>
2017-08-25 15:45:36 +0530

    function_class_one

--- poc/controllers/loginController.js
+++ poc/controllers/loginController.js
@@ -1,9 +1,14 @@
 app.controller("loginController",loginController);
-function loginController($scope,loginService) {
+function loginController($scope,loginService,$localStorage,$location) {
     $scope.obj = {};
     $scope.login = function () {
           loginService.authenticate($scope.obj).then(function (res) {
-            console.log(res);
+            if(res.data.login == "success"){
+                $localStorage.poc = res;
+                $location.path("/home");
+            }else{
+                alert("Login Fail !");
+            }
           });
     };
-};
+}
