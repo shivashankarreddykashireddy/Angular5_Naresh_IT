5a7204f31df2aa098c3a0235997052ce1fe247a0
revathi <revathigoury@gmail.com>
2017-09-15 06:27:00 +0530

    Classe

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {NgStyleDemo} from './app.ngstyle';
+import {NgModelDemo} from './app.ngmodel';
+import {FormsModule} from "@angular/forms";
+import {NgForDemo} from "./app.for";
+
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleDemo
+    NgStyleDemo,
+    NgModelDemo,
+    NgForDemo
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [NgStyleDemo]
+  bootstrap: [NgForDemo]
 })
 export class AppModule { }
