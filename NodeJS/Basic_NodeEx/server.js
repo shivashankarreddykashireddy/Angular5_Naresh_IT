9c292d4c14489471105113b76f73009c923972f2
Samba <samba.pendela@gmail.com>
2017-09-19 19:34:44 +0530

    Deleted Old Batch Files

--- NodeJS/Basic_NodeEx/server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-
-var app = express();
-
-app.use(express.static(__dirname+"/../Basic_NodeEx"));
-
-app.listen(8080);
-console.log("Server Listening the port no.8080");
--- POC/controllers/contactController.js
+++
@@ -1,6 +0,0 @@
-app.controller("contactController",contactController);
-function contactController($scope,homeService) {
-    homeService.getMySQLData().then(function (res) {
-        $scope.result = res;
-    });
-};
