a2d04b7fda1351772ea17e48dda00341f295b101
samba <samba.pendela@gmail.com>
2017-09-21 20:36:35 +0530

    Variables

---
+++ Introduction/variables.js
@@ -0,0 +1,3 @@
+var data = "Welcome to Angular4";
+document.write(data);
+var var_one = 100;
---
+++ Introduction/variables.ts
@@ -0,0 +1,4 @@
+var data:string="Welcome to Angular4";
+document.write(data);
+
+let var_one:string=100;
