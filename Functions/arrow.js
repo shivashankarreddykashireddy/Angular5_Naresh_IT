3ad5d6d20ff0e9d12130fc35f939e41e6cd44a1e
samba <samba.pendela@gmail.com>
2017-09-23 20:26:09 +0530

    Done

--- Functions/arrow.js
+++ Functions/arrow.js
@@ -5,45 +5,36 @@ var fun_one = function () {
 fun_one();
 
 */
-
 /*var fun_one = ()=>{
     document.write("<h1>Arrow....!</h1>");
 }*/
 /*var fun_one = ()=>document.write("<h1>Arrow...!</h1>");*/
 //fun_one();
-
-
 /*
 var fun_one = function (arg1:number,arg2:number) {
     document.write("Addition.."+(arg1+arg2));
 };
 fun_one(100,100);
 */
-
-
 /*var fun_one = (arg1:number,arg2:number)=>{
     console.log(arg1+arg2);
 };
 fun_one(100,100);*/
-
-var fun_one = (x)=>{
-    if(typeof x=='number'){
+var fun_one = function (x) {
+    if (typeof x == 'number') {
         document.write("Number...!");
-    }else if(typeof x=='string'){
+    }
+    else if (typeof x == 'string') {
         document.write("String...!");
-    }else if(typeof x=='boolean'){
+    }
+    else if (typeof x == 'boolean') {
         document.write("Boolean...!");
-    }else{
+    }
+    else {
         document.write("UnKnown Type...!");
     }
-}
+};
 fun_one(100);
 fun_one("Hello...!");
 fun_one(true);
 fun_one([]);
-
-
-
-
-
-
